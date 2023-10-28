808a523146f4d656fc578ebdab7e7243f20c1180
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-28 18:47:43 -0400

    added starter code to js file

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+
+    this.addEventListener("click", function(event) {
+        event.preventDefault();
+        var button = event.target;
+        var timeBlock = button.parentElement;
+        var timeBlockId = timeBlock.id;
+        var textArea = timeBlock.children[1];
+        var textAreaValue = textArea.value;
+        localStorage.setItem(timeBlockId, textAreaValue);
+    });
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+    
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
